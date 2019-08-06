570302bf32eaf45e10a6fccc5356c203a80e9d5c
Kyle Scheuerlein <kyle@kylesureline.com>
2019-08-06 09:51:41 -0700

    rename earth variable to add more meaning

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,14 +1,14 @@
 export const age = (planet, seconds) => {
-  const earth = 31557600;
+  const earth_year_in_seconds = 31557600;
   const ORBIT_in_SECONDS = {
-    earth,
-    'mercury': earth * 0.2408467,
-    'venus': earth * 0.61519726,
-    'mars': earth * 1.8808158,
-    'jupiter': earth * 11.862615,
-    'saturn': earth * 29.447498,
-    'uranus': earth * 84.016846,
-    'neptune': earth * 164.79132
+    'earth': earth_year_in_seconds,
+    'mercury': earth_year_in_seconds * 0.2408467,
+    'venus': earth_year_in_seconds * 0.61519726,
+    'mars': earth_year_in_seconds * 1.8808158,
+    'jupiter': earth_year_in_seconds * 11.862615,
+    'saturn': earth_year_in_seconds * 29.447498,
+    'uranus': earth_year_in_seconds * 84.016846,
+    'neptune': earth_year_in_seconds * 164.79132
   }
   return Number((seconds / ORBIT_in_SECONDS[planet]).toFixed(2));
 };
