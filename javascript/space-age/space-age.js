55c49a4ef40461b09fa1539dfc09484d592f7a9a
Kyle Scheuerlein <kyle@kylesureline.com>
2019-08-06 10:14:49 -0700

    refactor space-age solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,14 +1,18 @@
 export const age = (planet, seconds) => {
   const earth_year_in_seconds = 31557600;
-  const ORBIT_in_SECONDS = {
-    'earth': earth_year_in_seconds,
-    'mercury': earth_year_in_seconds * 0.2408467,
-    'venus': earth_year_in_seconds * 0.61519726,
-    'mars': earth_year_in_seconds * 1.8808158,
-    'jupiter': earth_year_in_seconds * 11.862615,
-    'saturn': earth_year_in_seconds * 29.447498,
-    'uranus': earth_year_in_seconds * 84.016846,
-    'neptune': earth_year_in_seconds * 164.79132
+  const orbital_period_in_years = {
+    'earth': 1,
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132
   }
-  return Number((seconds / ORBIT_in_SECONDS[planet]).toFixed(2));
+
+  const orbital_period_in_seconds = earth_year_in_seconds * orbital_period_in_years[planet];
+  const orbital_period_in_earth_years = seconds / orbital_period_in_seconds;
+
+  return Number(orbital_period_in_earth_years.toFixed(2));
 };
