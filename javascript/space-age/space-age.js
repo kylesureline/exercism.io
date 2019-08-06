7bd70170fb20019879d271847ca05918d1b35d37
Kyle Scheuerlein <kyle@kylesureline.com>
2019-08-06 09:05:01 -0700

    refactor space-age solution to be better DX

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,13 +1,14 @@
 export const age = (planet, seconds) => {
+  const earth = 31557600;
   const planets = {
-    'earth': 31557600,
-    'mercury': 7600543.81992,
-    'venus': 19414149.052176,
-    'mars': 59354032.69008,
-    'jupiter': 374355659.124,
-    'saturn': 929292362.8848,
-    'uranus': 2651370019.3296,
-    'neptune': 5200418560.032
+    earth,
+    'mercury': earth * 0.2408467,
+    'venus': earth * 0.61519726,
+    'mars': earth * 1.8808158,
+    'jupiter': earth * 11.862615,
+    'saturn': earth * 29.447498,
+    'uranus': earth * 84.016846,
+    'neptune': earth * 164.79132
   }
-  return parseFloat((seconds / planets[planet]).toFixed(2));
+  return (Math.round((seconds / planets[planet]) * 100))/100;
 };
