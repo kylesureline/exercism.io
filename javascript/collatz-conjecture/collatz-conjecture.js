85256bd259ae6647dc174cf040a3f9948b0383b8
Kyle Scheuerlein <kyle@kylesureline.com>
2019-09-30 21:53:51 -0700

    refactor collatz using ternary operator

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,23 +1,14 @@
 export const steps = (number) => {
-  let count = -1;
 
-  const collatz = (number) => {
-    count++;
-    if(number === 1) {
-      return 1;
-    }
-    else if(number % 2 === 0) {
-      return collatz(number / 2);
-    } else if(number % 2 === 1) {
-      return collatz((3 * number) + 1);
-    }
+  const collatz = (number, count) => {
+    if(number === 1) return count;
+
+    return collatz(number % 2 === 0 ? number / 2 : 3 * number + 1, count + 1);
   };
 
   if(number <= 0) {
     throw new Error('Only positive numbers are allowed');
   }
 
-  collatz(number);
-  
-  return count;
+  return collatz(number, 0);
 };
